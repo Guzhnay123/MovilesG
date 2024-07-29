e3fabbf03452b969bab47bc7c896858a840bf9e5
Guzhnay123 <166138688+Guzhnay123@users.noreply.github.com>
2024-07-28 19:38:38 -0500

    add

--- 1.1-semana-6/1.1-TA-()/cliente/src/pages/employees/create.tsx
+++ 1.1-semana-6/1.1-TA-()/cliente/src/pages/employees/create.tsx
@@ -1,8 +1,6 @@
-import { Create, useForm } from "@refinedev/antd";
-import { Form, Input, Button } from "antd";
 
 // Modelo de datos de empleado
-type Employee = {
+export type Employee = {
   id: number;
   name: string;
   email: string;
@@ -11,31 +9,3 @@ type Employee = {
   salary: number;
 };
 
-export const EmployeeCreate = () => {
-  const { formProps, saveButtonProps } = useForm<Employee>({});
-
-  return (
-    <Create saveButtonProps={saveButtonProps}>
-      <Form {...formProps} layout="vertical">
-        <Form.Item name="name" label="Name" rules={[{ required: true, message: 'Please enter employee name' }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item name="email" label="Email" rules={[{ required: true, message: 'Please enter employee email' }, { type: 'email', message: 'Please enter a valid email address' }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item name="position" label="Position" rules={[{ required: true, message: 'Please enter employee position' }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item name="department" label="Department" rules={[{ required: true, message: 'Please enter employee department' }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item name="salary" label="Salary" rules={[{ required: true, message: 'Please enter employee salary' }, { type: 'number', message: 'Please enter a valid number' }]}>
-          <Input type="number" />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">Submit</Button>
-        </Form.Item>
-      </Form>
-    </Create>
-  );
-};
